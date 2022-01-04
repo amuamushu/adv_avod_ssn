c2807072169d5b1616280f7ddefe4d96114d3c89
amuamushu <amy.yay22@gmail.com>
2022-01-04 15:01:54 -0800

    Add log files.

--- scripts/offline_eval/kitti_native_eval/run_eval_05_iou.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval_05_iou.sh
@@ -5,6 +5,6 @@ set -e
 cd $1
 echo "$3" | tee -a ./$4_results_05_iou_$2.txt
 # ./evaluate_object_3d_offline_05_iou ~/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
-./evaluate_object_3d_offline_05_iou /data/kitti_avod/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
-echo $4_results_05_iou_$2.txt $5
+./scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline_05_iou /data/kitti_avod/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
+echo $4_results_05_iou_$2.txt $5 "print run_eval_05_iou.sh"
 cp $4_results_05_iou_$2.txt $5
