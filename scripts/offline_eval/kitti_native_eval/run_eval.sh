da3be8f0668165552bbdd64901b9a4d209eee0ee
amuamushu <amy.yay22@gmail.com>
2022-01-06 13:58:09 -0800

    FIXED BASH SCRIPTS

--- scripts/offline_eval/kitti_native_eval/run_eval.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -e
-
-cd $1
+echo $1 "first 1"
+cd $1 
+# ../teams/DSC180A_FA21_A00/a15/avod_data/kitti_avod/object/outputs/test/predictions_sin_rand_5.0_5/image/kitti_native_eval/
 echo "$3" | tee -a ./$4_results_$2.txt
 # ./evaluate_object_3d_offline ~/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
-./scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline ../teams/DSC180A_FA21_A00/a15/avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
+~/avod_ssn/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline ~/teams/DSC180A_FA21_A00/a15/avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
 echo $4_results_$2.txt $5 "print run_eval.sh"
-cp $4_results_$2.txt $5
+cd ~/avod_ssn
+cp -R $4_results_$2.txt $5
--- scripts/offline_eval/kitti_native_eval/run_eval_05_iou.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval_05_iou.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e
-
+echo $1
 cd $1
 echo "$3" | tee -a ./$4_results_05_iou_$2.txt
 # ./evaluate_object_3d_offline_05_iou ~/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
-./scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline_05_iou /data/kitti_avod/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
+~/avod_ssn/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline_05_iou ~/avod_ssn/data/kitti_avod/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
 echo $4_results_05_iou_$2.txt $5 "print run_eval_05_iou.sh"
-cp $4_results_05_iou_$2.txt $5
+cd ~/avod_ssn
+cp -R $4_results_05_iou_$2.txt $5
