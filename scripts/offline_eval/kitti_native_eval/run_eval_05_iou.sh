9db5144f32f088a46fa3bba5b3dabccbab0dbeb1
amuamushu <amy.yay22@gmail.com>
2022-01-27 20:43:46 -0800

    Update bash path.

--- scripts/offline_eval/kitti_native_eval/run_eval_05_iou.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval_05_iou.sh
@@ -5,5 +5,7 @@ cd $1
 echo "$3" | tee -a ./$4_results_05_iou_$2.txt
 # ./evaluate_object_3d_offline_05_iou ~/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
 ~/avod_ssn/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline_05_iou ~/avod_ssn/data/kitti_avod/object/training/label_2/ $2/$3 | tee -a ./$4_results_05_iou_$2.txt
+cwd=$(pwd)
 cd ~/avod_ssn
-cp -R $5$4_results_05_iou_$2.txt $5
+echo $cwd/$4_results_05_iou_$2.txt
+cp -R $cwd/$4_results_05_iou_$2.txt $5
