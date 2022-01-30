0c1e2b83874fc4a70dffb33c068ad9e75c2f1a03
amuamushu <amy.yay22@gmail.com>
2022-01-29 19:55:16 -0800

    Add cd to original directory.

--- scripts/offline_eval/kitti_native_eval/run_eval.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "HELLO THERE PPL"
 set -e
 cd $1 
 # ../teams/DSC180A_FA21_A00/a15/avod_data/kitti_avod/object/outputs/test/predictions_sin_rand_5.0_5/image/kitti_native_eval/
@@ -9,3 +8,4 @@ cwd=$(pwd)
 cd ~/avod_ssn
 echo $4_results_$2.txt
 cp -R $cwd/$4_results_$2.txt $5
+cd $cwd
