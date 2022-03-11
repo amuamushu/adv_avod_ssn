2fab0dfc234d2d30a759633426be82c9c9814f20
amuamushu <amy.yay22@gmail.com>
2022-03-10 17:02:49 -0800

    Update some team paths.

--- scripts/offline_eval/kitti_native_eval/run_eval.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
+repo=$(pwd)
 cd $1 
-# ../teams/DSC180A_FA21_A00/a15/avod_data/kitti_avod/object/outputs/test/predictions_sin_rand_5.0_5/image/kitti_native_eval/
-# ./evaluate_object_3d_offline ~/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
-~/avod_ssn/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline ~/teams/DSC180A_FA21_A00/a15/avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
+$repo/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline ../avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
 cwd=$(pwd)
 cd ~/avod_ssn
 cp -R $cwd/$4_results_$2.txt $5
