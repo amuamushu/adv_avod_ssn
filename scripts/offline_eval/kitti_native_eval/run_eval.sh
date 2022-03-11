d51c6c354eedefd52b234a779a5a100ff4d15d06
amuamushu <amy.yay22@gmail.com>
2022-03-10 21:40:48 -0800

    Fix bash script paths.

--- scripts/offline_eval/kitti_native_eval/run_eval.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 repo=$(pwd)
+prev=$(cd ../ && pwd)
 cd $1 
-$repo/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline ../avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
-cwd=$(pwd)
-cd ~/avod_ssn
-cp -R $cwd/$4_results_$2.txt $5
-cd $cwd
+$repo/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline $prev/avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
