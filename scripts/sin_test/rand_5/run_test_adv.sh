252ef14e4fb91aa3b659675071af692f9fe2ced3
amuamushu <amy.yay22@gmail.com>
2022-03-10 16:52:19 -0800

    Uncommented.

--- scripts/sin_test/rand_5/run_test_adv.sh
+++ scripts/sin_test/rand_5/run_test_adv.sh
@@ -5,28 +5,27 @@ CONFIG_MAIN=avod/configs/simple/test_adv.config
 CONFIG_CLEAN=avod/configs/simple/test_adv_clean.config
 CONFIG_EVALSIN=avod/configs/simple/rand_5/pyramid_cars_with_aug_simple_evalsin_rand_5.config
 OUTPUT_DIR=outputs
-# EVAL_CKPTS='60 90 120' 
 EVAL_CKPTS='-1'
 
-# echo "training an adversarial model"
-# python avod/experiments/run_training.py \
-#         --pipeline_config=${CONFIG_MAIN} \
-#         --data_split='train' \
-#         --output_dir=${OUTPUT_DIR}
+echo "training an adversarial model"
+python avod/experiments/run_training.py \
+        --pipeline_config=${CONFIG_MAIN} \
+        --data_split='train' \
+        --output_dir=${OUTPUT_DIR}
 
-# echo "inference on adversarial data"
-# python avod/experiments/run_inference.py \
-#         --experiment_config=${CONFIG_MAIN} \
-#         --data_split='val' \
-#         --ckpt_indices ${EVAL_CKPTS} \
-#         --output_dir=${OUTPUT_DIR}      
+echo "inference on adversarial data"
+python avod/experiments/run_inference.py \
+        --experiment_config=${CONFIG_MAIN} \
+        --data_split='val' \
+        --ckpt_indices ${EVAL_CKPTS} \
+        --output_dir=${OUTPUT_DIR}      
 
-# echo "inference on clean data"
-# python avod/experiments/run_inference.py \
-#         --experiment_config=${CONFIG_CLEAN} \
-#         --data_split='val' \
-#         --ckpt_indices ${EVAL_CKPTS} \
-#         --output_dir=${OUTPUT_DIR}      
+echo "inference on clean data"
+python avod/experiments/run_inference.py \
+        --experiment_config=${CONFIG_CLEAN} \
+        --data_split='val' \
+        --ckpt_indices ${EVAL_CKPTS} \
+        --output_dir=${OUTPUT_DIR}      
 
 echo "inference on ssn"
 # Eval data on validation set (SIN)
@@ -36,7 +35,7 @@ python avod/experiments/run_inference.py \
         --ckpt_indices ${EVAL_CKPTS} #\
         # --output_dir=${OUTPUT_DIR}      
 
-# echo "evaluate ssn results"
-# python ./utils_sin/sin_calc_avg_kitti_eval.py \
-#         --experiment_config=${CONFIG_EVALSIN} \
-#         --data_split='val'
+echo "evaluate ssn results"
+python ./utils_sin/sin_calc_avg_kitti_eval.py \
+        --experiment_config=${CONFIG_EVALSIN} \
+        --data_split='val'
