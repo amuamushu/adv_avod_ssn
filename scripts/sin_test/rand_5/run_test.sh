e9547cd2246bb6ea1f686c39acfd4672c7ec4230
amuamushu <amy.yay22@gmail.com>
2022-03-10 18:21:57 -0800

    Delete some more files.

--- scripts/sin_test/rand_5/run_test.sh
+++ scripts/sin_test/rand_5/run_test.sh
@@ -3,7 +3,7 @@
 # Choose GPU ID with CUDA_VISIBLE_DEVICES={id}
 CONFIG_MAIN=avod/configs/simple/test.config
 CONFIG_EVALSIN=avod/configs/simple/rand_5/evalsin_test.config
-EVAL_CKPTS='2'
+EVAL_CKPTS='-1'
 
 # Train model for clean data
 python avod/experiments/run_training.py \
@@ -14,15 +14,4 @@ echo "CLEAN RUN INFERENCE"
 python avod/experiments/run_inference.py \
         --experiment_config=${CONFIG_MAIN} \
         --data_split='val' \
-        --ckpt_indices ${EVAL_CKPTS}
-# echo "SIN RUN INFERENCE"
-# # Eval data on validation set (SIN)
-# python avod/experiments/run_inference.py \
-#         --experiment_config=${CONFIG_EVALSIN} \
-#         --data_split='val' \
-#         --ckpt_indices ${EVAL_CKPTS}
-#         # --output_dir=${OUTPUT_DIR} \        
-# echo "SIN CALC AVG KITTI EVAL"
-# python ./utils_sin/sin_calc_avg_kitti_eval.py \
-#         --experiment_config=${CONFIG_EVALSIN} \
-#         --data_split='val'        
+        --ckpt_indices ${EVAL_CKPTS}      
