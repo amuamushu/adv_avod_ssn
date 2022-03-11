7d16f14d193dbf95e4aeba6a724a019ea76a7433
amuamushu <amy.yay22@gmail.com>
2022-03-10 16:50:35 -0800

    Add notebook.

--- scripts/offline_eval/kitti_native_eval/run_eval.sh
+++ scripts/offline_eval/kitti_native_eval/run_eval.sh
@@ -6,5 +6,6 @@ cd $1
 ~/avod_ssn/scripts/offline_eval/kitti_native_eval/evaluate_object_3d_offline ~/teams/DSC180A_FA21_A00/a15/avod_data/Kitti/object/training/label_2/ $2/$3 | tee -a ./$4_results_$2.txt
 cwd=$(pwd)
 cd ~/avod_ssn
+echo $5 "hi"
 cp -R $cwd/$4_results_$2.txt $5
 cd $cwd
